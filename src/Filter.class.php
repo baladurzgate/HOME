84443210ad36f30bcae410cffb7e790abde7cc6a
Alexandre <bighal.cormier@gmail.com>
2014-08-12 04:01:16 +0200

    filtre des map

---
+++ src/Filter.class.php
@@ -0,0 +1,55 @@
+<!--FILTER-->
+<?php
+class Filter extends Component {
+	private $sign;
+	private $name;
+	private $value;
+	
+	//construction 
+    function __construct($name,$sign="==",$value){
+		parent::__construct('filter');
+		$this->sign=$sign;
+		$this->name=$name;
+		$this->value=$value;
+    }
+	
+	public function getSign(){
+		return $this->sign;
+	}
+	
+	public function compare($array){
+		if(isset($array[$this->name])){
+			$a  = $array[$this->name];
+			switch ($this->sign){
+				case '==':
+					return($a==$this->value);
+				break;
+				case '<=':
+					return($a<=$this->value);
+				break;
+				case '<':
+					return($a<$this->value);						
+				break;
+				case '>=':
+					return($a>=$this->value);						
+				break;
+				case '>':
+					return($a>$this->value);						
+				break;
+				case 'between':
+					return($a>$this->value[0]&&$a<$this->value[1]);						
+				break;
+				case 'contain':
+					return(strpos($a,$this->value)!==false);						
+				break;
+				case 'lack':
+					return(strpos($a,$this->value)==false);						
+				break;
+			}
+		}
+		return false;
+	}
+	
+
+}
+?>
