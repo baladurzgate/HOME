4872acebdf37c01f6ee2f5d1eafc5e1a6fdc14de
Alexandre <bighal.cormier@gmail.com>
2014-08-12 03:52:08 +0200

    Create Content.class.php

---
+++ src/Content.class.php
@@ -0,0 +1,10 @@
+<!--CONTENT-->
+<?php
+class Content extends WebObject {
+    public function __construct($name,$updateLog=true) {
+		parent::__construct($name,"content");
+		$this->initPaths();
+
+	}
+}
+?>
