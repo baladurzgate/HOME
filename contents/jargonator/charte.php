6b85ade469e5e3c1abb2362478eb3c85ef76486a
Alexandre <bighal.cormier@gmail.com>
2014-08-12 04:38:34 +0200

    Create charte.php

---
+++ contents/jargonator/charte.php
@@ -0,0 +1,8 @@
+<!--CHARTE-->
+<fieldset id="inscription">
+	<legend>Charte de Confidentialité</legend>
+	<div>we stock your email and password in a crypted file on our server (which mean we can't read it). </br>
+	Those and your username will be the only informations we conserve(until you decide to delete your acount)</br>
+	we do not share them with any company or gourvenemental organisations . You can consult , modify and delete those informations at any time. </div>
+	</br><?php echo $S->parsePageButton('nouveau_compte','r e t o u r');?></br>
+</fieldset>
