8a5f25990d709baa2b5b4c990a65f4bd73dc4475
Alexandre <bighal.cormier@gmail.com>
2014-08-12 03:51:30 +0200

    Create Page.class.php

---
+++ src/Page.class.php
@@ -0,0 +1,30 @@
+<!-- PAGE -->
+<?php
+class Page extends WebObject {
+	private $access;
+    public function __construct($name,$access='all',$updateLog=true) {
+		parent::__construct($name,"page");
+		$this->access=$access;
+	}
+	
+	public function getURL(){
+		$name = $this->getName();
+		$contentPath = $this->S->content->getClientPath('dir');
+		return $contentPath.$name.".php";
+	}
+	public function getINC(){
+		$name = $this->getName();
+		$contentPath = $this->S->content->getServerPath('dir');
+		return $contentPath.$name.".php";
+	}
+	public function getAccess(){
+		return $this->access();
+	}
+		
+	public function append(){
+		$name = $this->getName();
+		$contentPath = $this->S->content->getServerPath('dir');
+		include $contentPath.$name.".php";;
+	}
+}
+?>
