e399a554f1ee61eff654a1ec3392b0a399a27209
Alexandre <bighal.cormier@gmail.com>
2014-08-12 03:52:49 +0200

    Create Template.class.php

---
+++ src/Template.class.php
@@ -0,0 +1,12 @@
+<!--TEMPLATE-->
+<?php
+class Template extends WebObject {
+    public function __construct($name,$updateLog=true) {
+		parent::__construct($name,"template");
+		$this->initPaths();
+	}
+	public function append(){
+		include ($this->getServerPath('php'));
+	}
+}
+?>
