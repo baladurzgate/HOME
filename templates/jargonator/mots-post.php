9c83f58e9af76f8ae1622d382f8afc3a6c9eef37
Alexandre <bighal.cormier@gmail.com>
2014-08-12 04:59:56 +0200

    Create mots-post.php

---
+++ templates/jargonator/mots-post.php
@@ -0,0 +1,12 @@
+<!--MOTS-POST-->
+<!--
+	$output
+	$values
+	$serial
+-->
+<div id="<?php echo $serial;?>" class="mots">
+	<b><span id="channel-orthographe"><?php echo $outputs['orthographe'];?></span></b></br>  
+	<i><span id="channel-definition"><?php echo $outputs['definition'];?></span></i></br>
+	<!--<span id="channel-author"><?php //echo $outputs['author'];?></span></br>-->
+</div>
+</br></br>
