f48775c3a47c1e6972ab7878ae574f5ce7c39d0b
Alexandre <bighal.cormier@gmail.com>
2014-08-12 04:31:09 +0200

    page d'acceuil

---
+++ contents/jargonator/home.php
@@ -0,0 +1,7 @@
+<fieldset id="cadre">
+	<legend>bonjour</legend>
+<div>	En sciences du langage, un jargon est un parler propre aux représentants d'une profession ou d'une activité commune se caractérisant par un lexique spécialisé. C'est un sociolecte.</br>.</br>
+Du XIIIe au XVIIIe siècles, ce mot a été employé dans l'usage commun, entre autres acceptions, pour désigner des langages, jugés secrets ou difficiles à comprendre, de différents groupes de gens vivant plus ou moins en marge de la société (bandits, voleurs, mendiants, merciers ambulants, etc.).</br>
+Au XVIIIe siècle, il a peu à peu été supplanté dans cet emploi, sauf dans les écrits portant sur le jargon ancien, par le mot argot qui n'avait pas du tout, au départ, cette signification.</br></br>
+Le jargon a une fonction principalement utilitaire qui vise l'efficacité de l'expression, mais revêt également un caractère identitaire. Le jargon peut être incompréhensible ou difficilement compréhensible pour les non initiés, mais il ne s'est pas développé dans ce but.</div>
+</fieldset>
