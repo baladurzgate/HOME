a8b1fcc8323c15312234319433eded691967a6e2
Alexandre <bighal.cormier@gmail.com>
2014-08-12 04:37:30 +0200

    Create confirmation-compte.php

---
+++ contents/jargonator/confirmation-compte.php
@@ -0,0 +1,6 @@
+<!--CONFIRMATION-COMPTE-->
+<fieldset id="inscription">
+	<legend>confirmation</legend>
+	<div>votre compte à été crée ! Un email de confirmation vous à été envoyé pour l'activer</div>
+	</br><?php echo $S->parsePageButton('home','r e t o u r');?></br>
+</fieldset>
