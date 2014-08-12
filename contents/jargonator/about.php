2311c321345a416d759ef1931d4e5f15feae8390
Alexandre <bighal.cormier@gmail.com>
2014-08-12 04:31:42 +0200

    Create about.php

---
+++ contents/jargonator/about.php
@@ -0,0 +1,8 @@
+<!--ABOUT-->
+<fieldset id="cadre">
+	<legend>à propos</legend>
+		<span>AUTEURS :<i> <A HREF="mailto:macoucou@xavierklein.eu">X.K</A> & <A HREF="mailto:contact@alexandrecormier.fr">A.C</A></i></span></br>
+		<span>GIT : ...</span></br>
+		<span>TYPOS UTILISEES : <i>skeletor , autopia</i></span></br>
+</fieldset>
+</div>
