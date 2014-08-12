ab8c84537680079f957ccb0811d087a2d6ccb6b7
Alexandre <bighal.cormier@gmail.com>
2014-08-12 04:10:23 +0200

    compte

---
+++ src/Acount/UserAcount.class.php
@@ -0,0 +1,28 @@
+<!--USERACOUNT-->
+<?php
+class UserAcount extends Post{
+
+	private $acountSystem;
+	
+	//constructor
+    public function __construct($acountSystem=NULL){
+		parent::__construct("acount",$acountSystem);
+		$this->changeType('acount');
+		$this->accountSystem = $acountSystem;
+		$this->init();
+		$this->Log("new useracount","event");	
+
+    }
+	public function init(){
+
+	}
+
+	public function exist(){
+		if($this->acountSystem!==NULL){		
+			return $this->accountSystem->acountAllreadyExist($this);
+		}else{
+			return false;
+		}
+	}
+}
+?>
