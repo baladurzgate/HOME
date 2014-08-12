465ff91a5c7da1c8e96d19b85d3b95306b4660af
Alexandre <bighal.cormier@gmail.com>
2014-08-12 04:12:57 +0200

    fonction de dialogue avec les tables

---
+++ src/js/submit.fonction.js
@@ -0,0 +1,39 @@
+//---SUBMIT--
+function submit($req,$send,$formId,$feedbackId,$header){
+    var elem,params,xmlhttp,feedback;
+	params ='request='+$req+'&';
+	feedback=document.getElementById($feedbackId);
+	if($req!=='logout'){
+		elem = document.getElementById($formId).elements;
+		for(var i = 0; i < elem.length; i++){
+			if (elem[i].tagName == "SELECT"){
+				params += elem[i].name + "=" + encodeURIComponent(elem[i].options[elem[i].selectedIndex].value) + "&";
+			}else{
+				params += elem[i].name + "=" + encodeURIComponent(elem[i].value) + "&";
+			}
+		} 
+	}
+	var xmlhttp;
+    if (window.XMLHttpRequest){// code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp=new XMLHttpRequest();
+    }else{// code for IE6, IE5
+        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.open("POST",$send,false);
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xmlhttp.onreadystatechange = function(){
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
+            if ( xmlhttp.responseText.trim() == "valid"){
+				if($header!=''){
+					window.location.href=$header;
+				}
+            }
+            else{
+				if($req!=='logout'){
+					feedback.innerHTML=xmlhttp.responseText;
+				}
+            }
+        }
+    }
+	xmlhttp.send(params);
+}
