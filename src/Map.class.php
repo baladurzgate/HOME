058ee887a3cf9d8c57119ff39b8460c9cc69dd57
Alexandre <bighal.cormier@gmail.com>
2014-08-12 03:57:13 +0200

    WIP, classe array amélioré

---
+++ src/Map.class.php
@@ -0,0 +1,31 @@
+<!-- MAP -->
+<?php
+class Map extends Component {
+	private $name;
+	private $map = array();
+    public function __construct($name) {
+		parent::__construct("cache");
+		$this->name=$name;
+	}
+	
+	public function set($name,$thing){
+		$this->map[$name]=$thing;
+	}
+	public function get($name){
+		if(isser($this->map[$name])){
+			return $this->map[$name];
+		}
+	}
+
+	public function getName(){
+		return $this->name;
+	}
+	public function getArray(){
+		return $this->map;
+	}
+	public function getLength(){
+		return count($this->map);
+	}
+
+}
+?>
