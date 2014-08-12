da9e384a9ab35bc43be48fc69a4b1a31c80d091b
Alexandre <bighal.cormier@gmail.com>
2014-08-12 04:11:20 +0200

    fonctions php utiles

---
+++ src/utils.php
@@ -0,0 +1,11 @@
+<!--UTILS-->
+<?php
+	$S=Site::getInstance();
+	function pageButton($page,$innerHTML,$class='bt'){
+		$url= $S->RequestURL($page);	
+		$onclick='window.location.href = "'.$url.'";';
+		$output='<button class="'.$class.'" onclick="'.$onclick.'">'.$innerHTML.'</button>'."\n";
+		return $output;
+	}
+
+?>
