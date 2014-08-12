3d2729686989000e9083922dd2f779207187e88b
Alexandre <bighal.cormier@gmail.com>
2014-08-12 04:33:19 +0200

    Create control.php

---
+++ contents/jargonator/control.php
@@ -0,0 +1,10 @@
+<!--CONTROL-->
+<fieldset id="inscription">
+	<legend>Vous n'êtes pas connecté</legend>
+<?php
+	echo $S->parsePageButton('nouveau_compte','I N S C R I P T I O N');
+	echo '--';
+	echo $S->parsePageButton('login','L O G I N');
+?>
+</fieldset>
+</div>
