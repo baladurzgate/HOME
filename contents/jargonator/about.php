936e9e9940dd257d5bc31e36eba1f4affd90f8c8
Alexandre <bighal.cormier@gmail.com>
2014-08-12 13:50:08 +0200

    ajout du liens vers le git

--- contents/jargonator/about.php
+++ contents/jargonator/about.php
@@ -2,7 +2,7 @@
 <fieldset id="cadre">
 	<legend>à propos</legend>
 		<span>AUTEURS :<i> <A HREF="mailto:macoucou@xavierklein.eu">X.K</A> & <A HREF="mailto:contact@alexandrecormier.fr">A.C</A></i></span></br>
-		<span>GIT : ...</span></br>
+		<span>GIT : <a href="https://github.com/baladurzgate/Home/tree/Jargonator">home/jargonator</a></span></br>
 		<span>TYPOS UTILISEES : <i>skeletor , autopia</i></span></br>
 </fieldset>
 </div>
