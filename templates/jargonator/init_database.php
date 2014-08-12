025cf742ae500a62a840c9fb120132e44541ec5b
Alexandre <bighal.cormier@gmail.com>
2014-08-12 04:53:40 +0200

    Create init_database.php

---
+++ templates/jargonator/init_database.php
@@ -0,0 +1,61 @@
+<!--INIT_DATABASE-->
+<?php
+	//BASE DE DONNEE 
+	$db1 = new DataBase('jargonator');
+	$db1->create('xml');
+	
+	//COMPTES UTILISATEUR
+	$comptes=new AcountSystem('comptes');
+	$db1->addTable($comptes);
+	
+	//TABLEAU MOTS
+	$mots = new Table("mots");
+	if(!$mots->allreadyExist()){
+		$channels = array();
+		$channels['orthographe']=new Channel("orthographe",array("type"=>"hidden","access"=>"form-archives","mandatory"=>"true"));
+		$channels['definition']=new Channel("definition",array("type"=>"text","access"=>"form-archives","mandatory"=>"true"));
+		$mots ->addchannels($channels);
+	}
+	$mots->linkTo($comptes);
+	$db1->addTable($mots);
+	
+	//TABLEAU RACINES
+	$racines = new Table("racines");
+	if(!$racines->allreadyExist()){
+		$channels = array();
+		$channels['orthographe']=new Channel("orthographe",array("type"=>"hidden","access"=>"none","mandatory"=>"true"));
+		$channels['definition']=new Channel("definition",array("type"=>"text","access"=>"form-archives","mandatory"=>"true"));
+		$racines ->addchannels($channels);
+	}
+	$racines->linkTo($comptes);
+	$db1->addTable($racines);
+	
+	//OPERATIONS DE CONTROL 
+	$User = $comptes->getCurrentUser(); 
+	
+	$controledPage=$_PAGE_INC;
+	$restricted_pages_list=array('contribuer','nouveau_mots');
+	
+	if($User==false){
+		$controledPage = $S->controlAccess($restricted_pages_list,'control');
+	}
+	
+	global $_PAGE_NAME;
+	
+	//urls du site
+	global $urls;
+	$urls=array();
+	$urls['nouveau_compte'] = $S->parseURLFor('nouveau_compte');
+	$urls['contribuer'] = $S->parseURLFor('contribuer');
+	$urls['archives'] = $S->parseURLFor('archives');
+	$urls['login'] = $S->parseURLFor('login');
+	$urls['nouveau_mots'] = $S->parseURLFor('nouveau_mots');
+	$urls['about'] = $S->parseURLFor('about');
+	$urls['charte'] = $S->parseURLFor('charte');
+	
+	//liste des pages nécéssitant de se logger
+
+	
+
+
+?>
