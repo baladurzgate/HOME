159c8362ca6f29af97be04b139969ed8e8a706df
Alexandre <bighal.cormier@gmail.com>
2014-08-12 03:12:48 +0200

    Create HEAD.php

---
+++ HEAD.php
@@ -0,0 +1,21 @@
+<!--HEAD-->
+<html lang="fr">
+<head>
+	<title><?php echo $S->getName();?></title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<link rel="shortcut icon" href="" />
+	<link rel="icon" type="image/x-icon" href="" />
+	<link rel="icon" type="image/png" href="" />
+	<meta name="keywords" content="" />
+	<meta name="description" content="" />
+	<meta name="geo.placename" content="Paris, France" />
+	<link rel="apple-touch-icon" href="" />
+	<meta name="viewport" content="width=1400 initial-scale=1.0" />
+	<link rel="stylesheet" type="text/css" href="<?php echo $_TEMPLATE_LINK.'/style.css'?>" />
+	<script>
+		console.log('------------!phpLog!----------');
+			<?php //echo $S->Log->getJsConsole(); ?>
+		console.log('------------------------------');
+	</script>
+</head>
+<body>
