1846d5a4877befb26feafab8d7baabdaea2885e4
Alexandre <bighal.cormier@gmail.com>
2014-08-12 04:32:42 +0200

    Create contribuer.php

---
+++ contents/jargonator/contribuer.php
@@ -0,0 +1,7 @@
+<!--CONTRIBUER-->
+<fieldset id="inscription">
+	<legend>Contribuer</legend>
+	<?php echo $S->parsePageButton('nouveau_mots','_____N O U V E A U___M O T S____');?></br></br>
+	<?php echo $S->parsePageButton('nouvelle_racine','___N O U V E L L E__R A C I N E___');?></br>
+</fieldset>
+</div>
