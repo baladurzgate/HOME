c5d697b2a1a81310cf4445f295a89fcee5630ac4
Alexandre <bighal.cormier@gmail.com>
2014-08-13 08:49:53 +0200

    +etym

--- templates/jargonator/mots-post.php
+++ templates/jargonator/mots-post.php
@@ -6,7 +6,8 @@
 -->
 <div id="<?php echo $serial;?>" class="mots">
 	<b><span id="channel-orthographe"><?php echo $outputs['orthographe'];?></span></b></br>  
-	<i><span id="channel-definition"><?php echo $outputs['definition'];?></span></i></br>
+	etym:<i><span id="channel-etymologie"><?php echo $outputs['etymologie'];?></span></i></br>  
+	def:<i><span id="channel-definition"><?php echo $outputs['definition'];?></span></i></br>
 	<!--<span id="channel-author"><?php //echo $outputs['author'];?></span></br>-->
 </div>
 </br></br>
