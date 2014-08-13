e5a8f4ee28f2e2fbf5a2e56d74c2af9453f81abc
Alexandre <bighal.cormier@gmail.com>
2014-08-13 07:24:26 +0200

    Update init_database.php

--- templates/jargonator/init_database.php
+++ templates/jargonator/init_database.php
@@ -1,14 +1,18 @@
-<!--INIT_DATABASE-->
 <?php
+	//<!--INIT_DATABASE-->
+	
 	//BASE DE DONNEE 
+	global $db;
 	$db1 = new DataBase('jargonator');
 	$db1->create('xml');
 	
 	//COMPTES UTILISATEUR
+	global $comptes;
 	$comptes=new AcountSystem('comptes');
 	$db1->addTable($comptes);
 	
 	//TABLEAU MOTS
+	global $mots;
 	$mots = new Table("mots");
 	if(!$mots->allreadyExist()){
 		$channels = array();
@@ -20,11 +24,13 @@
 	$db1->addTable($mots);
 	
 	//TABLEAU RACINES
+	global $racines;
 	$racines = new Table("racines");
 	if(!$racines->allreadyExist()){
 		$channels = array();
-		$channels['orthographe']=new Channel("orthographe",array("type"=>"hidden","access"=>"none","mandatory"=>"true"));
+		$channels['orthographe']=new Channel("orthographe",array("type"=>"hidden","access"=>"form-archives","mandatory"=>"true"));
 		$channels['definition']=new Channel("definition",array("type"=>"text","access"=>"form-archives","mandatory"=>"true"));
+		$channels['type']=new Channel("type",array("type"=>"hidden","access"=>"form-archives","mandatory"=>"true"));
 		$racines ->addchannels($channels);
 	}
 	$racines->linkTo($comptes);
@@ -54,8 +60,7 @@
 	$urls['charte'] = $S->parseURLFor('charte');
 	
 	//liste des pages nécéssitant de se logger
-
 	
-
-
+	$mots->extractMap();
+	$racines->extractMap();
 ?>
