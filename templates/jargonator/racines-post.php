ca127ff2f26b322e0a5e73b59ee0691f44ddfb5f
Alexandre <bighal.cormier@gmail.com>
2014-08-13 07:25:10 +0200

    Create racines-post.php

---
+++ templates/jargonator/racines-post.php
@@ -0,0 +1,13 @@
+<!--RACINE-POST-->
+<!--
+	$output
+	$values
+	$serial
+-->
+<div id="<?php echo $serial;?>" class="mots">
+	<b><span id="channel-orthographe"><?php echo $outputs['orthographe'];?></span></b></br>  
+	type : <i><span id="channel-author"><?php echo $outputs['type'];?></span></br></i>
+	sens :<i><span id="channel-definition"><?php echo $outputs['definition'];?></span></i></br>
+
+</div>
+</br></br>
