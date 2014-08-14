b755111a14ddd3c6419b795bb07b37743c45ec1a
Alexandre <bighal.cormier@gmail.com>
2014-08-14 03:27:49 +0200

    fonctions arrayToString et stringToArray ajoutés

--- src/utils.php
+++ src/utils.php
@@ -1,5 +1,5 @@
-<?php
-//<!--UTILS-->
+<?php 
+	//--UTILS--
 	$S=Site::getInstance();
 	function pageButton($page,$innerHTML,$class='bt'){
 		$url= $S->RequestURL($page);	
@@ -7,5 +7,33 @@
 		$output='<button class="'.$class.'" onclick="'.$onclick.'">'.$innerHTML.'</button>'."\n";
 		return $output;
 	}
-
+	
+	function stringToArray($string,$split1,$split2){
+		$output = array();
+		if($string!==NULL){
+			$varSplit = explode($split1,$string);
+			foreach($varSplit as $v){
+				if($v!==NULL||$v!==''){
+					$equalSplit = explode($split2,$v);
+					if(count($equalSplit)>1){
+						$output[$equalSplit[0]]=$equalSplit[1];
+					}else{
+						$output[$equalSplit[0]]="";
+					}
+				}
+			}
+			return $output;
+		}
+		return false;
+	} 
+	function arrayToString($array,$split1,$split2){
+		$string="";
+		if(is_array($array)==true){
+			foreach($array as $n => $v){
+				$string.=$n.$split2.$v.$split1;
+			}
+			$output = substr($string, 0, -1);	
+		}
+		return $output;
+	}
 ?>
