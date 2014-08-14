a856fd93da57628025d81d561e7bf123abea2601
Alexandre <bighal.cormier@gmail.com>
2014-08-15 01:36:17 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,20 @@
-<?php
+<?php 
 	//<!--INDEX-->
 	//le fichier le plus important du site :
 	require('init.php');
 	
 	//style et meta données
 	require('HEAD.php');
-?><body>
+?>
 		<div id="site">
 			<?php 
 				if(file_exists($_TEMPLATE_PHP)){
-					include($_TEMPLATE_PHP);
+					include_once($_TEMPLATE_PHP);
 				}else{
 				
 				}
 			?>
 		</div>
-		
-	</body>
 <?php 
 	//les balises scripts 
 	require($root.'/FOOT.php')
